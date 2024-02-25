b177738341e53932034c1c11d300265bda74fc72
tyler logue <tylerlogue3@gmail.com>
2024-02-25 18:43:06 -0500

    updates

--- csce242/project/part4.5/menu/script.js
+++ csce242/project/part4.5/menu/script.js
@@ -6,7 +6,7 @@ window.onload = () => {
     document.getElementById("hamburger").onclick = toggleHamburger;
 };
 
-
+/*popup from w3 school*/
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
