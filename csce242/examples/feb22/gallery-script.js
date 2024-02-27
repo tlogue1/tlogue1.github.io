08cddb711ead6d6d5f2f9e73be500f134da6c48f
tyler logue <tylerlogue3@gmail.com>
2024-02-27 11:43:25 -0500

    stuff

--- csce242/examples/feb22/gallery-script.js
+++ csce242/examples/feb22/gallery-script.js
@@ -3,5 +3,22 @@ document.querySelectorAll(".items section").forEach((section) =>{
         document.getElementById("dialog").style.display = "block";
 
         document.querySelector("#dialog-content img").src = e.currentTarget.querySelector("img").src;
+
+       const detailsSection = document.getElementById("dialog-details");
+       detailsSection.innerHTML = "";
+        const h3 = document.createElement("h3");
+        h3.innerHTML = section.querySelector("h3").innerHTML;
+        detailsSection.append(h3);
+        const p = document.createElement("p");
+        p.innerHTML = section.querySelector('p').innerHTML;
+        detailsSection.append(p);
+
     };
 });
+
+
+document.getElementById("dialog-close").onclick = () => {
+    document.getElementById("dialog").style.display = "none";
+
+};
+//onclick="document.getElementById('dialog').style.display='none'"
