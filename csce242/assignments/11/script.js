c1f6b6538dae42f41de080c86c06e4f872f0a601
tyler logue <tylerlogue3@gmail.com>
2024-03-01 19:16:33 -0500

    assignment 11

--- csce242/assignments/11/script.js
+++ csce242/assignments/11/script.js
@@ -12,30 +12,20 @@ class Tree {
     get item() {
         const section = document.createElement("section");
         section.classList.add("tree");
-
         const h3 = document.createElement("h3");
         h3.innerHTML = this.type;
         section.append(h3);
-
-        
-        const arrow = document.createElement("a");
-        arrow.href = "#";
-        arrow.innerHTML = "&#x2964";
-        arrow.classList.add("arrow");
-        h3.append(arrow);
-        arrow.onclick = this.expandContract;
-        
-
+        section.addEventListener("click", () => {
+            this.displayDetails(section);
+        });
         const columns = document.createElement("section");
         columns.classList.add("columns");
         section.append(columns);
-
         const imageSection = document.createElement("section");
         columns.append(imageSection);
         const image = document.createElement("img");
         image.src = "images/" + this.pic;
         imageSection.append(image);
-
         const moreSection = document.createElement("section");
         moreSection.classList.add("hidden" , "more");
         moreSection.append(this.paragraph("Type", this.type));
@@ -49,28 +39,25 @@ class Tree {
         return section;
 
     }
-
-    expandContract(e) {
-        const section = e.currentTarget.closest(".tree").querySelector(".more");
-
-        if(section.classList.contains("hidden")) {
-            e.currentTarget.innerHTML = "&#x2962";
-        } else {
-            e.currentTarget.innerHTML = "&#x2964";
-        }
-
-        section.classList.toggle("hidden");
-    };
-
     paragraph(type, info) {
         const p = document.createElement("p");
         p.innerHTML = `<strong>${type}:</strong> ${info}`
         return p;
     }
 
+    displayDetails(clickedSection) {
+        const treesSection = document.getElementById("trees");
+        Array.from(treesSection.children).forEach((section) => {
+            if(section !== clickedSection) {
+                section.querySelector(".more").classList.add("hidden");
+            }
+        });
+
+        const moreSection = clickedSection.querySelector(".more");
+        moreSection.classList.toggle("hidden");
+    }
 
 }
-
 const trees = [];
 trees.push(new Tree("Maple", "12 inch per year", "90 feet", "400 years", "Most are in Asia, but some are in North America, Europe, and North Africa","Maple trees take at least 30 years to product syrup" ,"maple-tree.jpeg"));
 trees.push(new Tree("Oak", "1 to 3 feet per year", "35 to 100 feet ", "600 years", "They grow througout most of the Eastern United States","Oak trees produce about 10 million acorns during its lifetime" , "oak-tree.jpeg"));
@@ -81,3 +68,5 @@ trees.forEach((tree) => {
     document.getElementById("trees").append(tree.item);
 
 });
+
+    
